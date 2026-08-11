70894d92ec5efb9fed728173afe5bb9081244869
chirag-s00 <201961331+chirag-s00@users.noreply.github.com>
2026-08-11 14:05:11 +0530

    feat: add zod validation for vault

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,50 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
+import { config } from 'dotenv'
+
+config({ path: '.env.local' })
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
-};
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=()',
+          },
+//           {
+//           key: "Content-Security-Policy",
+//           value: [
+//   "default-src 'self'",
+//   "script-src 'self' 'unsafe-inline'",
+//   "style-src 'self' 'unsafe-inline'",
+//   "img-src 'self' data: blob:",
+//   "font-src 'self'",
+//   "connect-src 'self'",
+//   "frame-src 'self'",
+//   "object-src 'none'",
+//   "base-uri 'self'",
+//   "frame-ancestors 'none'",
+// ].join('; '),
+//           },
+        ],
+      },
+    ]
+  },
+}
 
-export default nextConfig;
+export default nextConfig
